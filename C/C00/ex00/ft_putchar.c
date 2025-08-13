6579b33ed8b230370d66f3b49fda375fff06709b
goloubsource <alexander-taube@gmx.de>
2025-08-13 21:12:31 +0200

    Update ft_putchar.c

--- C/C00/ex00/ft_putchar.c
+++ C/C00/ex00/ft_putchar.c
@@ -16,3 +16,15 @@ void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
+
+*/void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+		ft_putchar(letter++);
+}
+/*
