6d35f7d609d69e0ba08c8c8db36c20067e7448ec
goloubsource <alexander-taube@gmx.de>
2025-08-13 21:20:02 +0200

    Update MAC.sh

--- Shell/Shell01/ex04/MAC.sh
+++ Shell/Shell01/ex04/MAC.sh
@@ -1,1 +1,6 @@
+#!bin/sh
 ifconfig | grep -E -o ..:..:..:..:..:..
+
+
+# Verwenden Sie "ifconfig", um die Mac-Adresse zu erhalten, um das Ergebnis von "ifconfig", "grep" mit "-E" zu festigen
+# und "-o", Argument "-E" ist für erweiterte regex, "-o" ist für das gleiche Muster, das Sie setzen. 
