4afd4dfe402d8d1db5b605c21c6a00f5f48f69d1
goloubsource <alexander-taube@gmx.de>
2025-08-13 21:10:08 +0200

    Update ft_print_comb.c

--- C/C00/ex05/ft_print_comb.c
+++ C/C00/ex05/ft_print_comb.c
@@ -39,3 +39,49 @@ void	ft_print_comb(void)
 		a++;
 	}
 }
+
+
+*/#include <unistd.h>
+#include <string.h>
+
+void	verify_numbers(int hundred, int dicker, int unit);
+void	print_numbers(int hundred, int dicker, int unit);
+
+void	ft_print_comb(void)
+{
+	verify_numbers(0, 1, 2);
+}
+
+void	verify_numbers(int hundred, int dicker, int unit)
+{
+	while (hundred <= 7)
+	{
+		while (dicker <= 8)
+		{
+			while (unit <= 9)
+			{
+				print_numbers(hundred, dicker, unit);
+				unit++;
+				if (hundred != 7)
+					write(1, ", ", 2);
+			}
+			unit = ++dicker + 1;
+		}
+		dicker = ++hundred;
+	}
+}
+
+void	print_numbers(int hundred, int dicker, int unit)
+{
+	char	h;
+	char	d;
+	char	u;
+
+	h = (char)(hundred + 48);
+	d = (char)(dicker + 48);
+	u = (char)(unit + 48);
+	write(1, &h, 1);
+	write(1, &d, 1);
+	write(1, &u, 1);
+}
+/*
