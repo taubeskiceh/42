e28cf34eaea1426dde0c1bdbdde31dd264a5a0df
goloubsource <alexander-taube@gmx.de>
2025-08-13 21:23:36 +0200

    Update MAC.sh

--- Shell/Shell01/ex04/MAC.sh
+++ Shell/Shell01/ex04/MAC.sh
@@ -1,6 +1,20 @@
 #!bin/sh
-ifconfig | grep -E -o ..:..:..:..:..:..
+ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'
 
 
-# Verwenden Sie "ifconfig", um die Mac-Adresse zu erhalten, um das Ergebnis von "ifconfig", "grep" mit "-E" zu festigen
-# und "-o", Argument "-E" ist für erweiterte regex, "-o" ist für das gleiche Muster, das Sie setzen. 
+# ifconfig: Show the system network settings.
+
+
+# grep: Search by text correspondence.
+
+
+# -o -E: Search in the text the correspondence of pattern.
+
+
+# ([[:xdigit:]]{1,2}:): Search for two hexadecimal digits followed by two-points.
+
+
+# {5}: The previous sequence should occur exactly five times.
+
+
+# [[:xdigit:]]{1,2}: Search the sequence of hexadecimal digits now without two points at the end.
